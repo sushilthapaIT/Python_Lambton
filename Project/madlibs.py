afb66ba1ecb05ec8cfc281be60f6ac54b3a066ff
lcf2332 <150272972+lcf2332@users.noreply.github.com>
2023-12-24 21:12:14 -0800

    1
    -string concatenation in Python.

---
+++ Project/madlibs.py
@@ -0,0 +1,13 @@
+#madlibs
+#Practise 1
+
+name = input("Enter Your Name: ")
+age = input("Enter Your Age: ")
+country = input("Enter Your Country: ")
+favFood = input("Enter Your Favorite Food: ")
+favSinger = input("Enter Your Favorite Singer: ")
+year =  input("Enter Your Date of birth year: ")
+
+madlibs = (f"Hi everyone, my name is {name} and i am from {country}. My age is {age} born in {year}.\n My favorite food is {favFood} and I like to listen to {favSinger}. \n Thank you everyone.")
+
+print(madlibs)
